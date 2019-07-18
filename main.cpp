bf7923e0584d005861521485cbf945d40d9783a8
vincgonzo <vincgonzo@hotmail.fr>
2019-07-18 16:04:56 +0200

    SDL window init functions

--- main.cpp
+++ main.cpp
@@ -8,17 +8,41 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
 
-int main ( int argc, char** argv )
-{
+void pause();
 
-    if (SDL_Init(SDL_INIT_VIDEO) == -1) // Démarrage de la SDL. Si erreur :
+int main(int argc, char *argv[])
+{
+    SDL_Surface *ecran = NULL;
+
+    SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
+    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
+
+    if(ecran == NULL)
     {
-        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); // Écriture de l'erreur
-        exit(EXIT_FAILURE); // On quitte le programme
+        fprintf(stderr, "Impossible de charger le mode video : %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
     }
 
+    SDL_WM_SetCaption("That is my window Game", NULL);
+    pause(); // Mise en pause du programme
 
-    SDL_Quit();
+    SDL_Quit(); // Arrêt de la SDL
 
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS; // Fermeture du programme
+}
+
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
 }
