08dda144f787a4e372bcb2360d8a0d17f9ab4934
vincgonzo <vincgonzo@hotmail.fr>
2019-07-22 10:41:01 +0200

    img src

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
     SDL_WM_SetCaption("Chargement d'images en SDL", NULL);
 
     /* Chargement d'une image Bitmap dans une surface */
-    imageDeFond = SDL_LoadBMP("lac_en_montagne.bmp");
+    imageDeFond = SDL_LoadBMP("pack_images_sdz/lac_en_montagne.bmp");
     /* On blitte par-dessus l'écran */
     SDL_BlitSurface(imageDeFond, NULL, ecran, &positionFond);
 
@@ -35,4 +35,20 @@ int main(int argc, char *argv[])
     SDL_Quit(); // Arrêt de la SDL
 
     return EXIT_SUCCESS; // Fermeture du programme
+}
+
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
 }
