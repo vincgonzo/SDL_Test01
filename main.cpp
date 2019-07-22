d5a9794e81ba48cbe94932deb0088dfb53fd6d41
vincgonzo <vincgonzo@hotmail.fr>
2019-07-22 15:40:43 +0200

    event click close window event

--- main.cpp
+++ main.cpp
@@ -13,50 +13,24 @@ void pause();
 int main(int argc, char *argv[])
 {
     SDL_Surface *ecran = NULL;
-    SDL_Surface *rectangle = NULL;
+    SDL_Event event; /* La variable contenant l'événement */
+    int continuer = 1; /* Notre booléen pour la boucle */
 
-    SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
-    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
+    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Rect position;
+    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+    SDL_WM_SetCaption("Gestion des evenements en SDL", NULL);
 
-    position.x = (640/2) - (220/2);
-    position.y = (480/2) - (220/2);
-    rectangle = SDL_CreateRGBSurface(SDL_HWSURFACE, 220, 180, 32, 0, 0, 0, 0);
-
-    if(ecran == NULL)
+    while (continuer) /* TANT QUE la variable ne vaut pas 0 */
     {
-        fprintf(stderr, "Impossible de charger le mode video : %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-
-    SDL_WM_SetCaption("That is my window Game", NULL);
-
-    SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 17, 206, 112));
-    SDL_FillRect(rectangle, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
-    SDL_BlitSurface(rectangle, NULL, ecran, &position);
-
-    SDL_Flip(ecran);
-    pause(); // Mise en pause du programme
-
-    SDL_FreeSurface(rectangle);
-    SDL_Quit(); // Arrêt de la SDL
-
-    return EXIT_SUCCESS; // Fermeture du programme
-}
-
-void pause()
-{
-    int continuer = 1;
-    SDL_Event event;
-
-    while (continuer)
-    {
-        SDL_WaitEvent(&event);
-        switch(event.type)
+        SDL_WaitEvent(&event); /* On attend un événement qu'on récupère dans event */
+        switch(event.type) /* On teste le type d'événement */
         {
-            case SDL_QUIT:
-                continuer = 0;
+            case SDL_QUIT: /* Si c'est un événement QUITTER */
+                continuer = 0; /* On met le booléen à 0, donc la boucle va s'arrêter */
+                break;
         }
     }
+
+    return EXIT_SUCCESS;
 }
