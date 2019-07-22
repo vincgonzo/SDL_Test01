75c152652e1c0af23ddfd9dc888982268a9fbcd2
vincgonzo <vincgonzo@hotmail.fr>
2019-07-22 10:48:44 +0200

    img insert into background img

--- main.cpp
+++ main.cpp
@@ -12,26 +12,33 @@ void pause();
 
 int main(int argc, char *argv[])
 {
-    SDL_Surface *ecran = NULL, *imageDeFond = NULL;
-    SDL_Rect positionFond;
+    SDL_Surface *ecran = NULL, *imageDeFond = NULL, *zozor = NULL;
+    SDL_Rect positionFond, positionZozor;
 
     positionFond.x = 0;
     positionFond.y = 0;
+    positionZozor.x = 500;
+    positionZozor.y = 260;
 
     SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_WM_SetIcon(SDL_LoadBMP("images/sdl_icone.bmp"), NULL);
 
     ecran = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE);
     SDL_WM_SetCaption("Chargement d'images en SDL", NULL);
 
-    /* Chargement d'une image Bitmap dans une surface */
-    imageDeFond = SDL_LoadBMP("pack_images_sdz/lac_en_montagne.bmp");
-    /* On blitte par-dessus l'écran */
+    imageDeFond = SDL_LoadBMP("images/lac_en_montagne.bmp");
     SDL_BlitSurface(imageDeFond, NULL, ecran, &positionFond);
+
+    /* Chargement et blittage de Zozor sur la scène */
+    zozor = SDL_LoadBMP("images/zozor.bmp");
+    SDL_BlitSurface(zozor, NULL, ecran, &positionZozor);
 
     SDL_Flip(ecran);
     pause();
 
     SDL_FreeSurface(imageDeFond);
+    SDL_FreeSurface(zozor);
     SDL_Quit(); // Arrêt de la SDL
 
     return EXIT_SUCCESS; // Fermeture du programme
